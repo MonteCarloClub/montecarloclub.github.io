27f2c0adeac5ff94ed6d264ad9be74446c27829e
jindada1 <zou_406143883@qq.com>
2022-05-15 11:28:41 +0800

    BE Deploy #1

--- be/assets/Index.ad80583a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.adb5cee7.js";import{o as _,c}from"./vendor.cab5376a.js";const r={};function a(n,o){return _(),c("h1",null,"Blocks")}var d=e(r,[["render",a],["__scopeId","data-v-0c56765d"]]);export{d as default};
